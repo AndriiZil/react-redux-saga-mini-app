3797229a2ba3f0646abb578ceeff3c766fbfc251
Andrii Zilnyk <andrii.zilnyk>
2021-08-18 13:12:52 +0300

    finished

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,10 +1,23 @@
-export default function Details(props) {
-    const { match: { params } } = props;
+import { selectPeopleDetails } from '../redux/reducers/peopleDetails/selectors';
+import { useSelector } from 'react-redux';
+import Loader from '../components/Loader';
+
+export default function Details() {
+    const peopleDetails = useSelector(selectPeopleDetails);
+
+    if (peopleDetails.loading) {
+        return <Loader />
+    }
+
+    const { name, birth_year, skin_color, mass, height } = peopleDetails.data;
 
     return (
         <div>
-            Details page
-            {JSON.stringify(params, null, 2)}
+            <h2>{name}</h2>
+            <h4>{birth_year}</h4>
+            <p>Skin: {skin_color}</p>
+            <p>Mass: {mass}</p>
+            <p>Height: {height}</p>
         </div>
     );
 }
--- src/redux/sagas/people/index.js
+++ src/redux/sagas/people/index.js
@@ -1,27 +1,59 @@
 import { call, apply, put, takeEvery, take, select, fork } from 'redux-saga/effects';
-import { LOAD_USERS, LOAD_USERS_SUCCESS } from '../../reducers/people/actions';
+import {LOAD_USERS, LOAD_USERS_FAILURE, LOAD_USERS_SUCCESS} from '../../reducers/people/actions';
 import { LOCATION_CHANGE } from 'connected-react-router';
 import { selectPeople } from '../../reducers/people/selectors';
 import { matchPath } from 'react-router';
 import { getRouteConfig, MAIN_ROUTE, PEOPLE_DETAILS_ROUTE } from '../../../routes';
-import { LOAD_USER_DETAILS } from '../../reducers/peopleDetails/actions';
+import {
+    LOAD_USER_DETAILS,
+    LOAD_USER_DETAILS_FAILURE,
+    LOAD_USER_DETAILS_SUCCESS
+} from '../../reducers/peopleDetails/actions';
 
-export function* loadPeopleDetails() {
+export function* loadPeopleDetails({ payload }) {
+    const { id } = payload;
 
+    try {
+        const request = yield call(
+            fetch,
+            `https://swapi.dev/api/people/${id}`
+        );
+
+        const people = yield apply(request, request.json);
+
+        yield put({
+            type: LOAD_USER_DETAILS_SUCCESS,
+            payload: people,
+        });
+    } catch (err) {
+        yield put({
+            type: LOAD_USER_DETAILS_FAILURE,
+            payload: err,
+        });
+    }
 }
 
 export function* loadPeopleList({ payload }) {
     const { page, search } = payload;
-    const request = yield call(
-        fetch,
-        `https://swapi.dev/api/people?page=${page}&search=${search}`
-    );
-    const data = yield apply(request, request.json);
 
-    yield put({
-        type: LOAD_USERS_SUCCESS,
-        payload: data,
-    });
+    try {
+        const request = yield call(
+            fetch,
+            `https://swapi.dev/api/people?page=${page}&search=${search}`
+        );
+        const data = yield apply(request, request.json);
+
+        yield put({
+            type: LOAD_USERS_SUCCESS,
+            payload: data,
+        });
+    } catch (err) {
+        yield put({
+            type: LOAD_USERS_FAILURE,
+            payload: err,
+        });
+    }
+
 }
 
 export function* routeChangeSaga() {
@@ -54,5 +86,6 @@ export function* routeChangeSaga() {
 
 export default function* peopleSaga() {
     yield fork(routeChangeSaga);
-    yield takeEvery(LOAD_USERS, loadPeopleList)
+    yield takeEvery(LOAD_USERS, loadPeopleList);
+    yield takeEvery(LOAD_USER_DETAILS, loadPeopleDetails);
 }
